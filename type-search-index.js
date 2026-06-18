84f3a793c01c1455907deadb4d3b48a3996e73e1
Susana Salas Martínez <ssalmar2509@g.educaand.es>
2026-06-18 11:44:41 +0200

    Versión con javadoc

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Circulo"},{"p":"<Unnamed>","l":"Circulos"},{"p":"<Unnamed>","l":"GestorFiguras"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Rectangulo"},{"p":"<Unnamed>","l":"Triangulo"}];updateSearchResults();
